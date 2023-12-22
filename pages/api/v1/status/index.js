4422cc3ef195270f8a497504ff725acd87e11351
Kleiton Barone Semolini <kleitonpolar6@gmail.com>
2023-12-22 22:32:31 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,9 +4,11 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const postgresVersion = await database.query("SHOW server_version;");
   const maxDatabaseConnections = await database.query("SHOW max_connections;");
-  const currentDatabaseConnections = await database.query(
-    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
-  );
+  const databaseName = process.env.POSTGRES_DB;
+  const currentDatabaseConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   response.status(200).send({
     updated_at: updatedAt,
     dependencies: {
