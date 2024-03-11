e865f5927445874f75303d7878b09cf5629a7e95
Kleiton Barone <25616578+KleitonBarone@users.noreply.github.com>
2024-03-11 23:25:30 +0000

    add post method to  endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,14 +2,31 @@ import migrationsRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migrations = await migrationsRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
+  if (request.method === "GET") {
+    const migrations = await migrationsRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
 
-  response.status(200).send(migrations);
+    response.status(200).send(migrations);
+  }
+
+  if (request.method === "POST") {
+    const migrations = await migrationsRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    response.status(200).send(migrations);
+  }
+
+  response.status(405).end();
 }
