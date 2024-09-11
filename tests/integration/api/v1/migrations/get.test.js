77b833ea73b08ebd32a65e2ad3fa46d06f9ac978
Kleiton Barone <25616578+KleitonBarone@users.noreply.github.com>
2024-09-11 21:58:56 +0000

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,21 +6,25 @@ beforeAll(async () => {
   await databaseUtil.cleanDatabase();
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieaving pending migrations", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/migrations");
 
-  const response1Body = await response1.json();
+      const response1Body = await response1.json();
 
-  expect(response1.status).toBe(200);
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
+      expect(response1.status).toBe(200);
+      expect(Array.isArray(response1Body)).toBe(true);
+      expect(response1Body.length).toBeGreaterThan(0);
 
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations");
+      const response2 = await fetch("http://localhost:3000/api/v1/migrations");
 
-  const response2Body = await response2.json();
+      const response2Body = await response2.json();
 
-  expect(response2.status).toBe(200);
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBeGreaterThan(0);
-  expect(response2Body.length).toBe(response1Body.length);
+      expect(response2.status).toBe(200);
+      expect(Array.isArray(response2Body)).toBe(true);
+      expect(response2Body.length).toBeGreaterThan(0);
+      expect(response2Body.length).toBe(response1Body.length);
+    });
+  });
 });
