e5096b49afb98ffdf5eb1d9969a60d7aa6e5185b
Kleiton Barone <25616578+KleitonBarone@users.noreply.github.com>
2025-02-02 19:00:12 +0000

    refactor: add padronized controller and errors

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,29 +2,14 @@ import { createRouter } from "next-connect";
 import database from "infra/database";
 import migrationsRunner from "node-pg-migrate";
 import { resolve } from "node:path";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import controller from "infra/controller";
 
 const router = createRouter();
 
 router.get(getHandler);
 router.post(postHandler);
 
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
-
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  return response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-  return response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
+export default router.handler(controller.errorHandlers);
 
 const defaultMigrationOptionsFactory = (dbClient, dryRun) => {
   return {
