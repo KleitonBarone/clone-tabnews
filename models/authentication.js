7ddcaf71158670b1a54b5bdfda43f44bad6b23fc
Kleiton Barone <25616578+KleitonBarone@users.noreply.github.com>
2025-07-10 02:05:24 +0000

    feat: implement authentication model

---
+++ models/authentication.js
@@ -0,0 +1,52 @@
+import { NotFoundError, UnauthorizedError } from "infra/errors";
+import user from "./user";
+import password from "./password";
+
+async function getAuthenticatedUser(providedEmail, providedPassword) {
+  try {
+    let storedUser;
+
+    try {
+      storedUser = await user.findOneByEmail(providedEmail);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw new UnauthorizedError({
+          message: "Email não confere.",
+          action: "Verifique se o email está correto.",
+          cause: error,
+        });
+      }
+
+      throw error;
+    }
+
+    const isPasswordMatch = await password.compare(
+      providedPassword,
+      storedUser.password,
+    );
+    if (!isPasswordMatch) {
+      throw new UnauthorizedError({
+        message: "Senha não confere.",
+        action: "Verifique se a senha está correta.",
+      });
+    }
+
+    return storedUser;
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      throw new UnauthorizedError({
+        message: "Dados de autorização não conferem.",
+        action: "Verifique se os dados enviados estão corretos.",
+        cause: error,
+      });
+    }
+
+    throw error;
+  }
+}
+
+const authentication = {
+  getAuthenticatedUser,
+};
+
+export default authentication;
--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
-import user from "models/user";
-import password from "models/password";
-import { UnauthorizedError } from "infra/errors";
+import authentication from "models/authentication";
 
 const router = createRouter();
 
@@ -12,23 +10,9 @@ export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
   const userInputValues = request.body;
-  try {
-    const storedUser = await user.findOneByEmail(userInputValues.email);
-    const isPasswordMatch = await password.compare(
-      userInputValues.password,
-      storedUser.password,
-    );
-    if (!isPasswordMatch) {
-      throw new UnauthorizedError({
-        message: "Senha não confere.",
-        action: "Verifique se a senha está correta.",
-      });
-    }
-  } catch (error) {
-    throw new UnauthorizedError({
-      message: "Dados de autorização não conferem.",
-      action: "Verifique se os dados enviados estão corretos.",
-    });
-  }
+  await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password,
+  );
   return response.status(201).json({});
 }
