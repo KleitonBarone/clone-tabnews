7842d93d0b40000887e3a82021e54627addf2359
Kleiton Barone <kleitonpolar6@gmail.com>
2025-11-15 16:58:03 +0000

    feat: implement activation user token with email link

--- models/activation.js
+++ models/activation.js
@@ -1,13 +1,64 @@
+import database from "infra/database";
 import email from "infra/email";
+import webserver from "infra/webserver";
 
-async function sendEmailtoUser(user) {
+const EXPIRATION_IN_MILLISECONDS = 15 * 60 * 1000; // 15 minutes
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+}
+
+async function findOneByUserId(userId) {
+  const tokenFound = await runSelectQuery(userId);
+  return tokenFound;
+}
+
+async function runSelectQuery(userId) {
+  const results = await database.query({
+    text: `
+      SELECT 
+        *
+      FROM 
+        user_activation_tokens
+      WHERE 
+        user_id = $1
+      ORDER BY 
+        created_at DESC
+      LIMIT 1
+      ;`,
+    values: [userId],
+  });
+
+  return results.rows[0];
+}
+
+async function runInsertQuery(userId, expiresAt) {
+  const results = await database.query({
+    text: `
+      INSERT INTO 
+        user_activation_tokens (user_id, expires_at) 
+      VALUES 
+        ($1, $2)
+      RETURNING
+        *
+      ;`,
+    values: [userId, expiresAt],
+  });
+
+  return results.rows[0];
+}
+
+async function sendEmailtoUser(user, activationToken) {
   await email.send({
     from: "TabNews <contato@exemplo.com>",
     to: user.email,
     subject: "Ative seu cadastro no TabNews!",
     text: `${user.username}, clique no link abaixo para ativar seu cadastro no TabNews:
 
-link-para-ativar-o-cadastro
+${webserver.origin}/cadastro/ativar/${activationToken.id}
 
 Atenciosamente,
 Equipe TabNews`,
@@ -16,6 +67,8 @@ Equipe TabNews`,
 
 const activation = {
   sendEmailtoUser,
+  create,
+  findOneByUserId,
 };
 
 export default activation;
