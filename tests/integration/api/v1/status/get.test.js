c3f58e83dfdafecc8b2d8d2ee6214fa18d62ec8e
Kleiton Barone <kleitonpolar6@gmail.com>
2026-06-14 19:45:13 -0300

    test: add default user coverage for status endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -25,6 +25,30 @@ describe("GET /api/v1/status", () => {
     });
   });
 
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const defaultUser = await orchestrator.createUserAndActivate();
+      const defaultUserSession = await orchestrator.createSession(defaultUser);
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          Cookie: `session_id=${defaultUserSession.token}`,
+        },
+      });
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.version).toBeUndefined();
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
+
   describe("Admin user", () => {
     test("Retrieving current system status", async () => {
       const adminUser = await orchestrator.createUserAndActivate();
